1a5c18f76544c584b04367a66fcc6f2f1a86e37a
Nicolas Alves <nicolasoalves1993@gmail.com>
2026-07-22 19:16:53 -0300

    Exercicios

---
+++ ex.py
@@ -0,0 +1,20 @@
+#numeros = [1, 2, 3, 4]
+
+#for numero in numeros:
+    
+    #nome = input('Digite um nome ')
+    #print(f' {numero} . {nome}')
+#print(numero)  
+
+#nomes = []
+
+#for i in range (4):
+    #nome = input('Digite um nome ')
+    #nomes.append(nome)
+    #print(nomes)
+
+#numeroTabuada = int(input('Digite o número que voce deseja '))
+#numeroMaximo = numeroTabuada * 10 + 1
+#for numeroTabuada in range( numeroTabuada, numeroMaximo, numeroTabuada):
+   
+ #print(numeroTabuada)
